35a1f1789953adb91403a3a0b5e779eac3d2a6f0
Caique Bezerra <caique.bezerra@gmail.com>
2021-09-29 18:42:37 -0300

    [Feature: Caique - Task 3 added UsuarioModel 29.09.2021]

--- src/main/java/com/empowerenergy/empowerenergy/model/UsuarioModel.java
+++ src/main/java/com/empowerenergy/empowerenergy/model/UsuarioModel.java
@@ -1,86 +1,80 @@
-package com.empowerenergy.empowrenergy.model;
+package com.empowerenergy.empowerenergy.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-
 @Entity
-@Table(name = "tb_usuario")
+@Table(name = "usuario")
 public class UsuarioModel {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long idUsuario;
-	
+
 	@NotBlank
-	@Size(min = 5, max = 50)
+	@Size(min = 5, max = 100)
 	private String nomeUsuario;
-	
+
 	@NotBlank
-	@Size(min = 5, max = 50)
+	@Size(min = 5, max = 100)
 	private String sobrenomeUsuario;
-	
-	@NotBlank
-	@Size(min = 5, max = 50)
-	private String emailUsuario;
-	
-	@NotBlank
-	@Size(min = 5, max = 50)
-	private String senhaUsuario;
-	
-	@NotBlank
+
 	private int idadeUsuario;
-	
-	
-	@OneToMany
-	//@JsonIgnoreProperties()
-	//private
-	
+
+	@Email
+	private String emailUsuario;
+
+	@NotBlank
+	@Size(min = 5, max = 100)
+	private String senhaUsuario;
+
 	public long getIdUsuario() {
 		return idUsuario;
 	}
+
 	public void setIdUsuario(long idUsuario) {
 		this.idUsuario = idUsuario;
 	}
+
 	public String getNomeUsuario() {
 		return nomeUsuario;
 	}
+
 	public void setNomeUsuario(String nomeUsuario) {
 		this.nomeUsuario = nomeUsuario;
 	}
+
 	public String getSobrenomeUsuario() {
 		return sobrenomeUsuario;
 	}
+
 	public void setSobrenomeUsuario(String sobrenomeUsuario) {
 		this.sobrenomeUsuario = sobrenomeUsuario;
 	}
-	public String getEmailUsuario() {
-		return emailUsuario;
-	}
-	public void setEmailUsuario(String emailUsuario) {
-		this.emailUsuario = emailUsuario;
-	}
-	public String getSenhaUsuario() {
-		return senhaUsuario;
-	}
-	public void setSenhaUsuario(String senhaUsuario) {
-		this.senhaUsuario = senhaUsuario;
-	}
+
 	public int getIdadeUsuario() {
 		return idadeUsuario;
 	}
+
 	public void setIdadeUsuario(int idadeUsuario) {
 		this.idadeUsuario = idadeUsuario;
 	}
-	
-	
-	
-	
-	
+
+	public String getEmailUsuario() {
+		return emailUsuario;
+	}
+
+	public void setEmailUsuario(String emailUsuario) {
+		this.emailUsuario = emailUsuario;
+	}
+
+	public String getSenhaUsuario() {
+		return senhaUsuario;
+	}
+
+	public void setSenhaUsuario(String senhaUsuario) {
+		this.senhaUsuario = senhaUsuario;
+	}
 }
