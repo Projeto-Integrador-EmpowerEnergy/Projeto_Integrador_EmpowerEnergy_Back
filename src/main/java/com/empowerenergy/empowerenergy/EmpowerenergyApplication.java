d4abf7b99455cce0f275a7723acbe462b2977729
ronanbedia <ronan.bedia@gmail.com>
2021-09-29 11:15:09 -0300

    [Feature: Ronan - Task 3 added Inicio do Projeto 29.09.2021]

---
+++ src/main/java/com/empowerenergy/empowerenergy/EmpowerenergyApplication.java
@@ -0,0 +1,13 @@
+package com.empowerenergy.empowerenergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpowerenergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpowerenergyApplication.class, args);
+	}
+
+}
