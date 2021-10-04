75e1a72ade5ecf0c19c59b3e0077510cdcce49ad
Evelyn <evelynarrudasilva@gmail.com>
2021-10-04 12:30:51 -0300

    [Feature Evelyn - Task: added findAllByTema - 04.10.21]

--- src/main/java/com/empowerenergy/empowerenergy/controller/TemaController.java
+++ src/main/java/com/empowerenergy/empowerenergy/controller/TemaController.java
@@ -2,10 +2,15 @@ package com.empowerenergy.empowerenergy.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,4 +35,9 @@ public class TemaController {
 		}
 				
 	}
+	
+	
 }
+	
+
+
