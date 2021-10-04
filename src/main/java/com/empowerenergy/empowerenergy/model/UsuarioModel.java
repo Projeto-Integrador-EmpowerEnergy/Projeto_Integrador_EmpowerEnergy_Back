371eec643337a71d13c470da2bd4fe9fdfc621a1
Lucas Reis <88865748+therealkaslow@users.noreply.github.com>
2021-10-04 10:05:56 -0300

    [Feature - Lucas - Task: modificado as linhas do usuarioModel]

--- src/main/java/com/empowerenergy/empowerenergy/model/UsuarioModel.java
+++ src/main/java/com/empowerenergy/empowerenergy/model/UsuarioModel.java
@@ -1,125 +1,89 @@
 package com.empowerenergy.empowerenergy.model;
 
-import javax.persistence.*;
-import javax.validation.constraints.Email;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-<<<<<<< HEAD:src/main/java/com/empowerenergy/empowrenergy/model/UsuarioModel.java
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-
-=======
->>>>>>> 601a8048d085d2906885e48c4f153db548272916:src/main/java/com/empowerenergy/empowerenergy/model/UsuarioModel.java
 @Entity
-@Table(name = "usuario")
+@Table(name = "tb_usuario")
 public class UsuarioModel {
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-<<<<<<< HEAD:src/main/java/com/empowerenergy/empowrenergy/model/UsuarioModel.java
-	private Long idUsuario;
-=======
 	private long idUsuario;
->>>>>>> 601a8048d085d2906885e48c4f153db548272916:src/main/java/com/empowerenergy/empowerenergy/model/UsuarioModel.java
-
-	@NotBlank
-	@Size(min = 5, max = 100)
-	private String nomeUsuario;
-
-	@NotBlank
-	@Size(min = 5, max = 100)
-	private String sobrenomeUsuario;
-
-<<<<<<< HEAD:src/main/java/com/empowerenergy/empowrenergy/model/UsuarioModel.java
+	
 	@NotBlank
 	@Size(min = 5, max = 50)
-=======
-	private int idadeUsuario;
-
-	@Email
->>>>>>> 601a8048d085d2906885e48c4f153db548272916:src/main/java/com/empowerenergy/empowerenergy/model/UsuarioModel.java
+	private String nomeUsuario;
+	
+	@NotBlank
+	@Size(min = 5, max = 50)
+	private String sobrenomeUsuario;
+	
+	@NotBlank
+	@Size(min = 5, max = 50)
 	private String emailUsuario;
-
+	
 	@NotBlank
-	@Size(min = 5, max = 100)
+	@Size(min = 5, max = 50)
 	private String senhaUsuario;
-
-<<<<<<< HEAD:src/main/java/com/empowerenergy/empowrenergy/model/UsuarioModel.java
+	
 	@NotBlank
 	private int idadeUsuario;
-
+	
+	
 	@OneToMany
-	@JsonIgnoreProperties
-	// private
-
-	public Long getIdUsuario() {
-		return idUsuario;
-	}
-
-	public void setIdUsuario(Long idUsuario) {
-=======
+	//@JsonIgnoreProperties()
+	//private
+	
 	public long getIdUsuario() {
 		return idUsuario;
 	}
-
 	public void setIdUsuario(long idUsuario) {
->>>>>>> 601a8048d085d2906885e48c4f153db548272916:src/main/java/com/empowerenergy/empowerenergy/model/UsuarioModel.java
 		this.idUsuario = idUsuario;
 	}
-
 	public String getNomeUsuario() {
 		return nomeUsuario;
 	}
-
 	public void setNomeUsuario(String nomeUsuario) {
 		this.nomeUsuario = nomeUsuario;
 	}
-
 	public String getSobrenomeUsuario() {
 		return sobrenomeUsuario;
 	}
-
 	public void setSobrenomeUsuario(String sobrenomeUsuario) {
 		this.sobrenomeUsuario = sobrenomeUsuario;
 	}
-
-<<<<<<< HEAD:src/main/java/com/empowerenergy/empowrenergy/model/UsuarioModel.java
-=======
-	public int getIdadeUsuario() {
-		return idadeUsuario;
-	}
-
-	public void setIdadeUsuario(int idadeUsuario) {
-		this.idadeUsuario = idadeUsuario;
-	}
-
->>>>>>> 601a8048d085d2906885e48c4f153db548272916:src/main/java/com/empowerenergy/empowerenergy/model/UsuarioModel.java
 	public String getEmailUsuario() {
 		return emailUsuario;
 	}
-
 	public void setEmailUsuario(String emailUsuario) {
 		this.emailUsuario = emailUsuario;
 	}
-
 	public String getSenhaUsuario() {
 		return senhaUsuario;
 	}
-
 	public void setSenhaUsuario(String senhaUsuario) {
 		this.senhaUsuario = senhaUsuario;
 	}
-<<<<<<< HEAD:src/main/java/com/empowerenergy/empowrenergy/model/UsuarioModel.java
-
 	public int getIdadeUsuario() {
 		return idadeUsuario;
 	}
-
 	public void setIdadeUsuario(int idadeUsuario) {
 		this.idadeUsuario = idadeUsuario;
 	}
-
-=======
->>>>>>> 601a8048d085d2906885e48c4f153db548272916:src/main/java/com/empowerenergy/empowerenergy/model/UsuarioModel.java
+	
+	
+	
+	
+	
 }
+
+	
+	
