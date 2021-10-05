d69e0806bf44c1b81d620ab541536f4a07b17759
Leonardo <lleonardo.azevedo@gmail.com>
2021-10-05 12:49:16 -0300

    [Feature Leonardo - Task PostagemRepository 05.10.2021]

---
+++ src/main/java/com/empowerenergy/empowerenergy/repository/PostagemRepository.java
@@ -0,0 +1,15 @@
+package com.empowerenergy.empowerenergy.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.empowerenergy.empowerenergy.model.PostagemModel;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<PostagemModel, Long> {
+
+	public List<PostagemModel> findAllBytituloContainingIgnoreCase(PostagemModel titulo); //ou tema?
+	
+}
